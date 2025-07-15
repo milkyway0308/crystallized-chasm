adfaafc2d92e6927e2e82e7efcc07e8093bc237e
skywolf46 <skywolf461@gmail.com>
2025-07-15 19:31:15 +0900

    bump: CRYS-PILL-1.2.0으로 버전 변경

--- redpill.user.js
+++ redpill.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Chasm Crystallized RedPill (결정화 캐즘 붉은약)
 // @namespace   https://github.com/milkyway0308/crystallized-chasm
-// @version     CRYS-PILL-v1.2.0-DEV
+// @version     CRYS-PILL-v1.2.0
 // @description 크랙의 통계 수정 및 데이터 표시 개선. 해당 유저 스크립트는 원본 캐즘과 호환되지 않음으로, 원본 캐즘과 결정화 캐즘 중 하나만 사용하십시오.
 // @author      chasm-js, milkyway0308
 // @match       https://crack.wrtn.ai/*
