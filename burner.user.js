6f61a3d9308b5bb91704dc87df9928c89867a6d8
skywolf46 <skywolf461@gmail.com>
2025-08-16 22:38:00 +0900

    bump: 버전 CRYS-BURN-v1.2.0으로 변경

--- burner.user.js
+++ burner.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Chasm Crystallized Burner+ (결정화 캐즘 버너+)
 // @namespace   https://github.com/chasm-js
-// @version     CRYS-BURN-v1.1.0
+// @version     CRYS-BURN-v1.2.0
 // @description 크랙 캐릭터 채팅 요약 및 반영. 해당 유저 스크립트는 원본 캐즘과 호환되지 않음으로, 원본 캐즘과 결정화 캐즘 중 하나만 사용하십시오.
 // @author      chasm-js, milkyway0308
 // @match       https://crack.wrtn.ai/*
