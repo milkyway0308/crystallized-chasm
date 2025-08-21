efc75b4540b75163670b09ff4db4b6675915b518
skywolf46 <skywolf461@gmail.com>
2025-08-22 05:30:50 +0900

    feat: 재시도 옵션 추가 및 모바일 환경 버튼 위치 변경

--- burner.user.js
+++ burner.user.js
@@ -13,7 +13,10 @@
 // ==/UserScript==
 
 GM_addStyle(
-  "@media screen and (max-width:500px) { .burner-test-button { display: none; } }"
+  ".burner-test-button { display: block; } " +
+    ".burner-input-button { display: none; } " +
+    "@media screen and (max-width:500px) { .burner-test-button { display: none; } }" +
+    "@media screen and (max-width:500px) { .burner-input-button { display: block; } }"
 );
 !(async function () {
   "use strict";
@@ -210,10 +213,11 @@ GM_addStyle(
       if (r?.data) {
         // Phase 2 - Consume event stream
         const result = await fetch(
-          `https://contents-api.wrtn.ai/character-chat/characters/chat/${this.json._id}/message/${a.data}?model=SONNET&platform=web&user=`,{
+          `https://contents-api.wrtn.ai/character-chat/characters/chat/${this.json._id}/message/${a.data}?model=SONNET&platform=web&user=`,
+          {
             headers: {
-              Authorization: `Bearer ${extractCookie("access_token")}`
-            }
+              Authorization: `Bearer ${extractCookie("access_token")}`,
+            },
           }
         );
         const reader = result.body.getReader();
@@ -538,13 +542,19 @@ GM_addStyle(
         o.modalBg
       }; color: ${
         o.textColor
-      }; display: none;">\n                            </div>\n                            <label style="display: flex; align-items: center; gap: 5px; font-size: 0.9em; color: ${
+      }; display: none;">\n                            </div>\n       <div style = "display: flex; flex-direction: row;">                      <label style="display: flex; align-items: center; gap: 5px; font-size: 0.9em; color: ${
         o.textColor
       }; margin-bottom: 15px;">\n                                <input id="cb-attach-usernote" type="checkbox" style="background: ${
         o.modalBg
       }; color: ${
         o.textColor
-      };">\n                                <span>유저노트 첨부</span>\n                            </label>\n                            <div style="display: flex; justify-content: space-between; align-items: center; margin-bottom: 5px;">\n                                <label style="font-size: 0.9em; color: ${
+      };">\n                                <span>유저노트 첨부</span>\n                            </label><label style="display: flex; align-items: center; gap: 5px; font-size: 0.9em; margin-left: 8px; color: ${
+        o.textColor
+      }; margin-bottom: 15px;">\n                                <input id="cb-auto-retry" type="checkbox" style="background: ${
+        o.modalBg
+      }; color: ${
+        o.textColor
+      };">\n                                <span>과부하시 자동 재시도</span>\n                            </label></div>\n                            <div style="display: flex; justify-content: space-between; align-items: center; margin-bottom: 5px;">\n                                <label style="font-size: 0.9em; color: ${
         o.textColor
       };">실행 로그</label>\n                                <div id="cb_timer" style="font-size: 0.9em; color: ${
         o.textColor
@@ -929,13 +939,45 @@ GM_addStyle(
               const o = `https://generativelanguage.googleapis.com/v1beta/models/${n}:generateContent?key=${e}`,
                 a = { contents: { parts: [{ text: t }] } };
               try {
-                const n = await fetch(o, {
+                let geminiResponse = await fetch(o, {
+                  method: "POST",
+                  headers: { "Content-Type": "application/json" },
+                  body: JSON.stringify(a),
+                });
+                let retryCount = 0;
+                while (
+                  !geminiResponse.ok &&
+                  document.getElementById("cb-auto-retry") &&
+                  document.getElementById("cb-auto-retry").checked
+                ) {
+                  if (geminiResponse.status !== 503) {
+                    T.value = `[${p()}] 과부하 이외의 문제로 Gemini API 요청이 실패하였습니다. \n${
+                      T.value
+                    }`;
+                    break;
+                  }
+                  T.value = `[${p()}] Gemini API 과부하 - 잠시 후 재시도합니다. 자동 재시도를 중단하려면 체크박스를 해제하세요. \n${
+                    T.value
+                  }`;
+                  await new Promise((resolve) =>
+                    setTimeout(
+                      resolve,
+                      500 +
+                        100 * Math.min(++retryCount, 10) +
+                        Math.random() * 1000
+                    )
+                  );
+
+                  T.value = `[${p()}] 다시 시도하는 중.. \n${T.value}`;
+                  geminiResponse = await fetch(o, {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
                     body: JSON.stringify(a),
-                  }),
-                  e = await n.json();
-                return n.ok
+                  });
+                }
+
+                e = await geminiResponse.json();
+                return geminiResponse.ok
                   ? e?.candidates?.[0]?.content?.parts?.[0]?.text || null
                   : (throwError(
                       new Error("Gemini API request failed"),
@@ -1148,8 +1190,7 @@ GM_addStyle(
       }),
       j.addEventListener("click", () => s.remove());
   }
-
-  async function injectButton() {
+  async function injectBannerButton() {
     const selected = document.getElementsByClassName("burner-test-button");
     if (selected && selected.length > 0) {
       return;
@@ -1169,14 +1210,37 @@ GM_addStyle(
       buttonCloned.addEventListener("click", C);
     }
   }
+
+  async function injectInputbutton() {
+    const selected = document.getElementsByClassName("burner-input-button");
+    if (selected && selected.length > 0) {
+      return;
+    }
+    // Top element
+    const data = document.getElementsByClassName("css-v66jlo");
+    if (data && data.length > 0) {
+      const top = data[0].parentElement;
+      const buttonCloned = top.childNodes[1].cloneNode(true);
+      buttonCloned.className = "burner-input-button " + buttonCloned.className;
+      // https://www.svgrepo.com/svg/521664/fire
+      buttonCloned.innerHTML =
+        '<svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="var(--icon_tertiary)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M10.0284 1.11813C9.69728 1.2952 9.53443 1.61638 9.49957 1.97965C9.48456 2.15538 9.46201 2.32986 9.43136 2.50363C9.3663 2.87248 9.24303 3.3937 9.01205 3.98313C8.5513 5.15891 7.67023 6.58926 5.96985 7.65195C3.57358 9.14956 2.68473 12.5146 3.06456 15.527C3.45234 18.6026 5.20871 21.7903 8.68375 22.9486C9.03 23.0641 9.41163 22.9817 9.67942 22.7337C10.0071 22.4303 10.0238 22.0282 9.94052 21.6223C9.87941 21.3244 9.74999 20.5785 9.74999 19.6875C9.74999 19.3992 9.76332 19.1034 9.79413 18.8068C10.3282 20.031 11.0522 20.9238 11.7758 21.5623C12.8522 22.5121 13.8694 22.8574 14.1722 22.9466C14.402 23.0143 14.6462 23.0185 14.8712 22.9284C17.5283 21.8656 19.2011 20.4232 20.1356 18.7742C21.068 17.1288 21.1993 15.3939 20.9907 13.8648C20.7833 12.3436 20.2354 10.9849 19.7537 10.0215C19.3894 9.29292 19.0534 8.77091 18.8992 8.54242C18.7101 8.26241 18.4637 8.04626 18.1128 8.00636C17.8332 7.97456 17.5531 8.06207 17.3413 8.24739L15.7763 9.61686C15.9107 7.44482 15.1466 5.61996 14.1982 4.24472C13.5095 3.24609 12.7237 2.47913 12.1151 1.96354C11.8094 1.70448 11.5443 1.50549 11.3525 1.36923C11.2564 1.30103 11.1784 1.24831 11.1224 1.21142C10.7908 0.99291 10.3931 0.923125 10.0284 1.11813ZM7.76396 20.256C7.75511 20.0744 7.74999 19.8842 7.74999 19.6875C7.75 18.6347 7.89677 17.3059 8.47802 16.0708C8.67271 15.6572 8.91614 15.254 9.21914 14.8753C9.47408 14.5566 9.89709 14.4248 10.2879 14.5423C10.6787 14.6598 10.959 15.003 10.9959 15.4094C11.2221 17.8977 12.2225 19.2892 13.099 20.0626C13.5469 20.4579 13.979 20.7056 14.292 20.8525C15.5 20.9999 17.8849 18.6892 18.3955 17.7882C19.0569 16.6211 19.1756 15.356 19.0091 14.1351C18.8146 12.7092 18.2304 11.3897 17.7656 10.5337L14.6585 13.2525C14.3033 13.5634 13.779 13.5835 13.401 13.3008C13.023 13.018 12.8942 12.5095 13.092 12.0809C14.4081 9.22933 13.655 6.97987 12.5518 5.38019C12.1138 4.74521 11.6209 4.21649 11.18 3.80695C11.0999 4.088 10.9997 4.39262 10.8742 4.71284C10.696 5.16755 10.4662 5.65531 10.1704 6.15187C9.50801 7.26379 8.51483 8.41987 7.02982 9.34797C5.57752 10.2556 4.71646 12.6406 5.04885 15.2768C5.29944 17.2643 6.20241 19.1244 7.76396 20.256Z" fill="var(--icon_tertiary)"></path> </g></svg>';
+
+      buttonCloned.removeAttribute("onClick");
+      buttonCloned.addEventListener("click", C);
+      top.insertBefore(buttonCloned, top.childNodes[top.childNodes.length - 1]);
+    }
+  }
+
+  async function injectButton() {
+    await injectBannerButton();
+    await injectInputbutton();
+  }
+
   async function addChasmButton() {
     if (!/\/u\/[a-f0-9]+\/c\/[a-f0-9]+/.test(location.pathname)) return;
     const n = document.querySelector(".css-uxwch2");
-    console.log(n);
     await injectButton();
-    console.log("Try to inject");
-    console.log(document.getElementById("chasmMenu"));
-    console.log(document.getElementById("chasmBurner"));
     if (
       n &&
       !document.getElementById("chasmMenu") &&
