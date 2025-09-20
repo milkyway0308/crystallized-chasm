d1eda34bf5f5d60ca6d1838880746c94f9e6ee61
skywolf46 <skywolf461@gmail.com>
2025-09-20 13:15:43 +0900

    bump: 버전 CRYS-AZRO-v1.0.1로 변경

--- absolutezero.user.js
+++ absolutezero.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Chasm Crystallized AbsoluteZero (결정화 캐즘 절대영도)
 // @namespace   https://github.com/milkyway0308/crystallized-chasm
-// @version     CRYS-AZRO-v1.0.0
+// @version     CRYS-AZRO-v1.0.1
 // @description 선택하거나 호버하지 않은 작품의 GIF 차단. 이 기능은 결정화 캐즘 오리지널 패치입니다.
 // @author      milkyway0308
 // @match       https://crack.wrtn.ai/*
