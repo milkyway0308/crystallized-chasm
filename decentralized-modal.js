0d57a142eb379e1872f5f9a5bbb8d77f34095f5b
skywolf46 <skywolf461@gmail.com>
2025-10-22 13:23:15 +0900

    fix(decentral.js): 누락된 펑션 추가

--- decentralized-modal.js
+++ decentralized-modal.js
@@ -781,6 +781,10 @@ const DECENTRAL_MENU_ICON_SVG = `
 <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <rect width="24" height="24" fill="none"></rect> <path d="M6 12H18" stroke="var(--decentral-text)" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M6 15.5H18" stroke="var(--decentral-text)" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M6 8.5H18" stroke="var(--decentral-text)" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
 `;
 
+// https://www.svgrepo.com/svg/521469/arrow-down
+const DECENTRAL_ARROW_ICON_SVG = `
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><!-- Uploaded to: SVG Repo, www.svgrepo.com, Transformed by: SVG Repo Mixer Tools --><svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#FFFFFF"><g id="SVGRepo_bgCarrier" stroke-width="0"/><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/><g id="SVGRepo_iconCarrier"> <path d="M5.70711 9.71069C5.31658 10.1012 5.31658 10.7344 5.70711 11.1249L10.5993 16.0123C11.3805 16.7927 12.6463 16.7924 13.4271 16.0117L18.3174 11.1213C18.708 10.7308 18.708 10.0976 18.3174 9.70708C17.9269 9.31655 17.2937 9.31655 16.9032 9.70708L12.7176 13.8927C12.3271 14.2833 11.6939 14.2832 11.3034 13.8927L7.12132 9.71069C6.7308 9.32016 6.09763 9.32016 5.70711 9.71069Z" fill="#FFFFFF"/> </g></svg>
+`;
 class ModalManager {
   static doesInit = false;
   /** @type {Map<string, ModalManager>} */
@@ -2380,7 +2384,9 @@ class ComponentAppender extends HTMLComponentConvertable {
         );
         option.append(
           setupStyleNode("div", "margin-left: auto;", (iconNode) => {
-            iconNode.append(createArrowIcon());
+            iconNode.append(setupNode("div", (node) => {
+              node.outerHTML = DECENTRAL_ARROW_ICON_SVG;
+            }));
           })
         );
         option.onclick = () => {
