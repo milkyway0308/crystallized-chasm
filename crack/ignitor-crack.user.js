0ac97ba627467ad6d04b9c2326fb45ca1ea35b16
skywolf46 <skywolf461@gmail.com>
2025-10-30 15:51:55 +0900

    bump(C2/IGNT/CRACK): 버전 CRAK-IGNT-v1.2.1로 변경

--- crack/ignitor-crack.user.js
+++ crack/ignitor-crack.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name        Crack Chasm Crystallized Ignitor (크랙 / 결정화 캐즘 점화기)
 // @namespace   https://github.com/milkyway0308/crystallized-chasm
-// @version     CRAK-IGNT-v1.2.0
+// @version     CRAK-IGNT-v1.2.1
 // @description 캐즘 버너의 기능 계승. 이 기능은 결정화 캐즘 오리지널 패치입니다. **기존 캐즘 버너 및 결정화 캐즘 버너+와 호환되지 않습니다. 버너 모듈을 제거하고 사용하세요.**
 // @author      milkyway0308
 // @match       https://crack.wrtn.ai/*
@@ -59,7 +59,7 @@ GM_addStyle(`
 
 !(async function () {
   const PLATFORM_SAVE_KEY = "chasm-ignt-settings";
-  const VERSION = "v1.2.0";
+  const VERSION = "v1.2.1";
 
   const { initializeApp } = await import(
     "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js"
@@ -1343,10 +1343,12 @@ GM_addStyle(`
               };
             },
             onChange: (_, text) => {
-              sessionStorage.setItem(
-                result.sessionId,
-                JSON.stringify(result.withResponse(text))
-              );
+              if (settings.saveModifiedResult) {
+                sessionStorage.setItem(
+                  result.sessionId,
+                  JSON.stringify(result.withResponse(text))
+                );
+              }
             },
             suffixModifier: (node) => {
               new ComponentAppender(node).addText(
