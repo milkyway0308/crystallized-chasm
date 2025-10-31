4e6697da4b89f383af3d0da0bd8d19f8a1dde58a
skywolf46 <skywolf461@gmail.com>
2025-10-31 18:14:12 +0900

    bump(C2/BABE/IGNT): 버전 BABE-IGNT-v1.0.8로 변경

--- babechat/ignitor-babe.user.js
+++ babechat/ignitor-babe.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name        BabeChat / Chasm Crystallized Ignitor (베이비챗 / 결정화 캐즘 점화기)
 // @namespace   https://github.com/milkyway0308/crystallized-chasm
-// @version     BABE-IGNT-v1.0.8p
+// @version     BABE-IGNT-v1.0.8
 // @description 캐즘 버너 및 애프터버너의 기능 계승. 이 기능은 결정화 캐즘 오리지널 패치입니다.
 // @author      milkyway0308
 // @match       https://babechat.ai/*
@@ -61,7 +61,7 @@ GM_addStyle(`
 
 !(async function () {
   const PLATFORM_SAVE_KEY = "chasm-babe-ignt-settings";
-  const VERSION = "v1.0.8p";
+  const VERSION = "v1.0.8";
 
   const { initializeApp } = await import(
     "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js"
