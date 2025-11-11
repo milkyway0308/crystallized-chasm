3238cf346afb49ab1b69359043b5c05f3e58f928
skywolf46 <skywolf461@gmail.com>
2025-11-12 06:54:19 +0900

    fix(C2/CRCK/TMI): 크랙의 모델명 업데이트 반영

--- tmi.user.js
+++ tmi.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Chasm Crystallized TMI (캐즘 과포화)
 // @namespace    https://github.com/milkyway0308/crystallized-chasm/
-// @version      CRYS-TMI-v1.5.1
+// @version      CRYS-TMI-v1.5.2
 // @description  크랙 UI에 추가 정보 제공. 이 기능은 결정화 캐즘 오리지널 패치입니다.
 // @author       milkyway0308
 // @match        https://crack.wrtn.ai/*
@@ -307,7 +307,7 @@
     } else if (currentColor === "text_tertiary") {
       expectedChatType = "일반챗";
     } else if (currentColor == "text_cracker_secondary") {
-      expectedChatType = "파워챗+";
+      expectedChatType = "프로챗 1.0";
     }
     let nextText = formatChatLeft(expectedChatType, cracker);
     if (nextText === textTag.textContent) {
@@ -346,7 +346,7 @@
       nextText = chatType + " | ∞";
     } else if (chatType === "파워챗") {
       nextText = chatType + " | 잔여 " + Math.floor(cracker / 15) + "회";
-    } else if (chatType === "파워챗+") {
+    } else if (chatType === "프로챗 1.0") {
       let expectedCracker = 45;
       const time = new Date();
       // 이벤트는 2025-11-06부터 11월 끝까지 지속됨.
