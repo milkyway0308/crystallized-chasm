8aa14a3b6c4abc3932a460205aabf53b84b75fe4
skywolf46 <skywolf461@gmail.com>
2025-11-28 18:41:41 +0900

    bump(C2/CRCK/IGNT): 버전 코드 변경

--- crack/ignitor-crack.user.js
+++ crack/ignitor-crack.user.js
@@ -65,7 +65,7 @@ GM_addStyle(`
 
 !(async function () {
   const PLATFORM_SAVE_KEY = "chasm-ignt-settings";
-  const VERSION = "v1.4.0";
+  const VERSION = "v1.4.2p";
   const { initializeApp } = await import(
     "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js"
   );
