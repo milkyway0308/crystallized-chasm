5e542151073d78a2b057c440dac36f2c0ad1de57
skywolf46 <skywolf461@gmail.com>
2025-12-05 19:27:09 +0900

    bump(C2): 모듈 버전 프리뷰에서 릴리즈로 격상

--- crack/ignitor-crack.user.js
+++ crack/ignitor-crack.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name        Crack Chasm Crystallized Ignitor (크랙 / 결정화 캐즘 점화기)
 // @namespace   https://github.com/milkyway0308/crystallized-chasm
-// @version     CRAK-IGNT-v1.4.2.3p
+// @version     CRAK-IGNT-v1.4.2
 // @description 캐즘 버너의 기능 계승. 이 기능은 결정화 캐즘 오리지널 패치입니다. **기존 캐즘 버너 및 결정화 캐즘 버너+와 호환되지 않습니다. 버너 모듈을 제거하고 사용하세요.**
 // @author      milkyway0308
 // @match       https://crack.wrtn.ai/*
@@ -65,7 +65,7 @@ GM_addStyle(`
 
 !(async function () {
   const PLATFORM_SAVE_KEY = "chasm-ignt-settings";
-  const VERSION = "v1.4.2.2p";
+  const VERSION = "v1.4.2";
   const { initializeApp } = await import(
     "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js"
   );
