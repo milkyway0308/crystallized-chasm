c1ec0d0632424f3d2760ad03cab41aee868dd881
skywolf46 <skywolf461@gmail.com>
2025-12-29 06:01:40 +0900

    fix(decentralized.js): 모달 포지션 조정

--- decentralized-modal.js
+++ decentralized-modal.js
@@ -65,7 +65,7 @@ const DECENTRAL_CSS_VALUES = `
         left: 0;
         width: 100%;
         height: 100%;
-        position: absolute;
+        position: fixed;
         font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
     }
         
