6d7a4d73ef3171412080edd8fc73577a03175d0e
skywolf46 <skywolf461@gmail.com>
2026-01-08 21:51:37 +0900

    fix(C2/CRCK/ALTR): 차원이동 모듈 초기 오류 수정

--- crack/alternation.user.js
+++ crack/alternation.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name        Chasm Crystallized Alternation (결정화 캐즘 차원이동)
 // @namespace   https://github.com/milkyway0308/crystallized-chasm
-// @version     CRYS-ALTR-v1.4.9
+// @version     CRYS-ALTR-v1.4.10
 // @description 채팅 로그 복사 및 새 채팅방으로 포크. 이 기능은 결정화 캐즘 오리지널 패치입니다.
 // @author      milkyway0308
 // @match       https://crack.wrtn.ai/*
@@ -1327,7 +1327,6 @@ if (!document.chasmApi) {
      * ToastifyInjector을 초기화합니다.
      */
     constructor() {
-      super();
       this.#init();
     }
 
