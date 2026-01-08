6f27ac682b01e1ff61b41f6981ce7b53f3d1aef5
skywolf46 <skywolf461@gmail.com>
2026-01-08 20:26:10 +0900

    fix(C2/CRCK/ALTR): 크랙 UI 업데이트 대응

--- alternation.user.js
+++ alternation.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name        Chasm Crystallized Alternation (결정화 캐즘 차원이동)
 // @namespace   https://github.com/milkyway0308/crystallized-chasm
-// @version     CRYS-ALTR-v1.4.6
+// @version     CRYS-ALTR-v1.4.7p
 // @description 채팅 로그 복사 및 새 채팅방으로 포크. 이 기능은 결정화 캐즘 오리지널 패치입니다.
 // @author      milkyway0308
 // @match       https://crack.wrtn.ai/*
@@ -12,19 +12,66 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-GM_addStyle(
-  "@keyframes chasm-rotate { from { transform: rotate(0deg); } to {  transform: rotate(360deg); }}" +
-    "@keyframes chasm-blinker { 70% { opacity: 0;} }" +
-    ".chasm-altr-button { font-size: 16px; line-height: 110%; font-weight: 500; }" +
-    'body[data-theme="light"] .chasm-altr-button { color: #1A1918; transition: color 0.1s ease;}' +
-    'body[data-theme="dark"] .chasm-altr-button { color: #F0EFEB; transition: color 0.1s ease;}' +
-    'body[data-theme="light"] .chasm-altr-button[disabled="true"] { color: #adadadff; transition: color 0.1s ease;}' +
-    'body[data-theme="dark"] .chasm-altr-button[disabled="true"] { color: #6e6e6eff; transition: color 0.1s ease;}' +
-    ".chasm-altr-button .warning-icon { display: none; margin-left: 40px; animation: chasm-blinker 1s infinite;}" +
-    '.chasm-altr-button[warning="true"] .warning-icon { display: block; }' +
-    ".chasm-altr-button .loading-icon { display: none; margin-left: 40px; animation: chasm-rotate 1s infinite; }" +
-    '.chasm-altr-button[loading="true"] .loading-icon { display: block; }'
-);
+GM_addStyle(`
+  @keyframes chasm-rotate {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes chasm-blinker {
+    70% {
+      opacity: 0;
+    }
+  }
+
+  body[data-theme="light"] {
+    --chasm-altr-color: #1A1918;
+    --chasm-altr-color-disabled: #adadadff;
+  }
+
+  body[data-theme="dark"] {
+    --chasm-altr-color: #F0EFEB;
+    --chasm-altr-color-disabled: #6e6e6eff;
+  }
+
+  .chasm-altr-button {
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 1;
+    transition: color 0.1s ease;
+    color: var(--chasm-altr-color);
+    padding-top: 12px;
+    user-select: none;
+    cursor: pointer;
+  }
+
+  .chasm-altr-button[disabled="true"] {
+    color: var(--chasm-altr-color-disabled);
+  }
+
+  .chasm-altr-button .warning-icon {
+    display: none;
+    margin-left: 40px;
+    animation: chasm-blinker 1s infinite;
+  }
+  .chasm-altr-button[warning="true"] .warning-icon {
+    display: block;
+  }
+
+  .chasm-altr-button .loading-icon {
+    display: none;
+    margin-left: 40px;
+    animation: chasm-rotate 1s infinite;
+  }
+
+  .chasm-altr-button[loading="true"] .loading-icon {
+    display: block;
+  }  
+`);
+
 if (!document.chasmApi) {
   document.chasmApi = {};
 }
@@ -797,12 +844,17 @@ if (!document.chasmApi) {
   }
   /**
    * 사이드 패널의 요소를 찾아 반환합니다.
-   * @returns {boolean} 사이드 패널 요소
+   * @returns {HTMLElement} 사이드 패널 요소
    */
   function extractSidePanel() {
-    return isDarkMode()
-      ? document.getElementsByClassName("css-uxwch2")[0]
-      : document.getElementsByClassName("css-uxwch2")[0];
+    const node = isDarkMode()
+      ? document.getElementsByClassName("css-c82bbp")[0]
+      : document.getElementsByClassName("css-c82bbp")[0];
+
+    if (node) {
+      return node.childNodes[0].childNodes[0];
+    }
+    return undefined;
   }
 
   /**
@@ -980,7 +1032,7 @@ if (!document.chasmApi) {
     // Text Container (Middle)
     const textContainer = document.createElement("div");
     textContainer.style.cssText =
-      "display: flex; flex-direction: column; margin-left: 12px;";
+      "display: flex; flex-direction: column; margin-left: 8px;";
     // Title
     const titleElement = document.createElement("p");
     titleElement.textContent = "평행세계로 이동";
@@ -990,7 +1042,7 @@ if (!document.chasmApi) {
     progressElement.textContent = "차원 이동 준비 완료";
     progressElement.className = "chasm-altr-description";
     progressElement.style.cssText =
-      "font-size: 11px; color: var(--text_tertiary);";
+      "font-size: 11px; color: var(--text_tertiary); margin-top: 4px;";
     textContainer.appendChild(progressElement);
 
     topContainer.append(textContainer);
@@ -1033,7 +1085,12 @@ if (!document.chasmApi) {
       )[0].textContent = `차원 이동 준비 완료`;
     });
     // button.childNodes[1].textContent = "평행세계로 이동";
-    panel.appendChild(button);
+    for (let element of panel.getElementsByTagName("p")) {
+      if (element.textContent === "시작설정") {
+        panel.insertBefore(button, element.previousSibling);
+        break;
+      }
+    }
   }
 
   function prepare() {
@@ -1161,7 +1218,7 @@ if (!document.chasmApi) {
     const element = document.createElement("div");
     // https://www.svgrepo.com/svg/321565/teleport
     element.innerHTML =
-      '<svg viewBox="0 0 512 512" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="var(--icon_secondary)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="var(--icon_secondary)" d="M249.334 22.717c-18.64 2.424-35.677 23.574-37.043 51.49v.02c-.057 1.186-.097 2.38-.097 3.59 0 16.362 5.658 30.827 13.942 40.818l10.127 12.213-15.592 2.933c-10.75 2.025-18.622 7.702-25.373 16.978-2.285 3.14-4.384 6.707-6.31 10.62-57.54-6.44-97.91-21.06-97.91-37.952 0-17.363 42.647-31.983 102.75-37.97-.213-2.51-.323-5.057-.323-7.636v-.002c0-.84.024-1.674.047-2.51-96.43 6.77-167.298 29.15-167.3 55.71-.002 25.33 64.462 46.86 154.074 54.67-.19.742-.394 1.465-.576 2.216-2.36 9.72-4.05 20.22-5.268 31.03-.01 0-.02 0-.03.002-.418 3.653-.78 7.34-1.095 11.046l.05-.005c-1.316 15.777-1.772 31.88-1.893 46.95h35.894l2.115 28.4c-68.24-4.994-118.444-21.004-118.444-39.843 0-13.243 24.83-24.89 63.27-32.33.3-4.056.66-8.115 1.076-12.162-76.42 9.353-129.17 29.168-129.172 52.086-.002 28.17 79.71 51.643 185.098 56.768l5.94 79.77c10.5 2.648 24.84 4.162 39.017 4.068 13.79-.092 27.235-1.71 36.45-4l5.263-79.846c105.308-5.14 184.935-28.605 184.935-56.76 0-23.013-53.196-42.895-130.13-52.2.304 4.02.557 8.047.755 12.07 38.883 7.43 63.965 19.17 63.965 32.536 0 18.84-49.804 34.85-117.908 39.844l1.87-28.402h34.18c-.012-15.113-.127-31.27-1.033-47.094.01 0 .02.002.032.004-.214-3.687-.472-7.352-.782-10.986l-.02-.002c-.94-11.157-2.367-21.984-4.546-31.967-.09-.405-.184-.803-.275-1.206 89.518-7.826 153.893-29.344 153.893-54.656 0-26.787-72.076-49.332-169.77-55.887.025.895.053 1.788.053 2.688 0 2.5-.104 4.97-.304 7.407 61.19 5.836 104.61 20.61 104.61 38.2 0 16.805-39.633 31.355-96.524 37.848-2.01-4.283-4.26-8.15-6.762-11.505-6.83-9.167-15.063-14.81-27.14-16.682l-15.913-2.47 10.037-12.59c6.928-8.69 11.912-20.715 13.057-34.268h.002c.163-1.95.25-3.93.25-5.938 0-.77-.022-1.532-.048-2.29-.015-.48-.033-.958-.057-1.434h-.002c-1.48-29.745-20.507-51.3-41.076-51.3-2.528 0-3.966-.087-4.03-.08h-.003zM194.54 355.822c-97.11 6.655-168.573 29.11-168.573 55.8 0 31.932 102.243 57.815 228.367 57.815S482.7 443.555 482.7 411.623c0-26.608-71.02-49.004-167.67-55.736l-.655 9.93c60.363 6.055 103.074 20.956 103.074 38.394 0 22.81-73.032 41.298-163.12 41.298-90.088 0-163.12-18.49-163.12-41.297 0-17.533 43.18-32.502 104.07-38.493l-.74-9.895z"></path></g></svg>';
+      '<svg viewBox="0 0 512 512" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="var(--icon_tertiary)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="var(--icon_tertiary)" d="M249.334 22.717c-18.64 2.424-35.677 23.574-37.043 51.49v.02c-.057 1.186-.097 2.38-.097 3.59 0 16.362 5.658 30.827 13.942 40.818l10.127 12.213-15.592 2.933c-10.75 2.025-18.622 7.702-25.373 16.978-2.285 3.14-4.384 6.707-6.31 10.62-57.54-6.44-97.91-21.06-97.91-37.952 0-17.363 42.647-31.983 102.75-37.97-.213-2.51-.323-5.057-.323-7.636v-.002c0-.84.024-1.674.047-2.51-96.43 6.77-167.298 29.15-167.3 55.71-.002 25.33 64.462 46.86 154.074 54.67-.19.742-.394 1.465-.576 2.216-2.36 9.72-4.05 20.22-5.268 31.03-.01 0-.02 0-.03.002-.418 3.653-.78 7.34-1.095 11.046l.05-.005c-1.316 15.777-1.772 31.88-1.893 46.95h35.894l2.115 28.4c-68.24-4.994-118.444-21.004-118.444-39.843 0-13.243 24.83-24.89 63.27-32.33.3-4.056.66-8.115 1.076-12.162-76.42 9.353-129.17 29.168-129.172 52.086-.002 28.17 79.71 51.643 185.098 56.768l5.94 79.77c10.5 2.648 24.84 4.162 39.017 4.068 13.79-.092 27.235-1.71 36.45-4l5.263-79.846c105.308-5.14 184.935-28.605 184.935-56.76 0-23.013-53.196-42.895-130.13-52.2.304 4.02.557 8.047.755 12.07 38.883 7.43 63.965 19.17 63.965 32.536 0 18.84-49.804 34.85-117.908 39.844l1.87-28.402h34.18c-.012-15.113-.127-31.27-1.033-47.094.01 0 .02.002.032.004-.214-3.687-.472-7.352-.782-10.986l-.02-.002c-.94-11.157-2.367-21.984-4.546-31.967-.09-.405-.184-.803-.275-1.206 89.518-7.826 153.893-29.344 153.893-54.656 0-26.787-72.076-49.332-169.77-55.887.025.895.053 1.788.053 2.688 0 2.5-.104 4.97-.304 7.407 61.19 5.836 104.61 20.61 104.61 38.2 0 16.805-39.633 31.355-96.524 37.848-2.01-4.283-4.26-8.15-6.762-11.505-6.83-9.167-15.063-14.81-27.14-16.682l-15.913-2.47 10.037-12.59c6.928-8.69 11.912-20.715 13.057-34.268h.002c.163-1.95.25-3.93.25-5.938 0-.77-.022-1.532-.048-2.29-.015-.48-.033-.958-.057-1.434h-.002c-1.48-29.745-20.507-51.3-41.076-51.3-2.528 0-3.966-.087-4.03-.08h-.003zM194.54 355.822c-97.11 6.655-168.573 29.11-168.573 55.8 0 31.932 102.243 57.815 228.367 57.815S482.7 443.555 482.7 411.623c0-26.608-71.02-49.004-167.67-55.736l-.655 9.93c60.363 6.055 103.074 20.956 103.074 38.394 0 22.81-73.032 41.298-163.12 41.298-90.088 0-163.12-18.49-163.12-41.297 0-17.533 43.18-32.502 104.07-38.493l-.74-9.895z"></path></g></svg>';
     element.style.cssText = "margin-top: 2px;";
     return element;
   }
@@ -1171,7 +1228,7 @@ if (!document.chasmApi) {
     element.classList.add("warning-icon");
     // https://www.svgrepo.com/svg/502912/warning-1
     element.innerHTML =
-      '<svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <circle cx="12" cy="17" r="1" fill="var(--icon_secondary)"></circle> <path d="M12 10L12 14" stroke="var(--icon_secondary)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M3.44722 18.1056L10.2111 4.57771C10.9482 3.10361 13.0518 3.10362 13.7889 4.57771L20.5528 18.1056C21.2177 19.4354 20.2507 21 18.7639 21H5.23607C3.7493 21 2.78231 19.4354 3.44722 18.1056Z" stroke="var(--icon_secondary)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>';
+      '<svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <circle cx="12" cy="17" r="1" fill="var(--icon_tertiary)"></circle> <path d="M12 10L12 14" stroke="var(--icon_tertiary)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M3.44722 18.1056L10.2111 4.57771C10.9482 3.10361 13.0518 3.10362 13.7889 4.57771L20.5528 18.1056C21.2177 19.4354 20.2507 21 18.7639 21H5.23607C3.7493 21 2.78231 19.4354 3.44722 18.1056Z" stroke="var(--icon_tertiary)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>';
     return element;
   }
 
@@ -1179,7 +1236,7 @@ if (!document.chasmApi) {
     const element = document.createElement("div");
     // https://www.svgrepo.com/svg/448500/loading
     element.innerHTML =
-      '<svg width="20px" height="20px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none" class="hds-flight-icon--animation-loading"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g fill="var(--icon_secondary)" fill-rule="evenodd" clip-rule="evenodd"> <path d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8z" opacity=".2"></path> <path d="M7.25.75A.75.75 0 018 0a8 8 0 018 8 .75.75 0 01-1.5 0A6.5 6.5 0 008 1.5a.75.75 0 01-.75-.75z"></path> </g> </g></svg>';
+      '<svg width="20px" height="20px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none" class="hds-flight-icon--animation-loading"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g fill="var(--icon_tertiary)" fill-rule="evenodd" clip-rule="evenodd"> <path d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8z" opacity=".2"></path> <path d="M7.25.75A.75.75 0 018 0a8 8 0 018 8 .75.75 0 01-1.5 0A6.5 6.5 0 008 1.5a.75.75 0 01-.75-.75z"></path> </g> </g></svg>';
     element.childNodes[0].classList.add("loading-icon");
     element.style.cssText = "margin-top: -3px";
     return element;
@@ -1196,7 +1253,7 @@ if (!document.chasmApi) {
         if (item.getAttribute("href") === "/setting") {
           const clonedElement = item.cloneNode(true);
           clonedElement.id = "chasm-decentral-menu";
-          const textElement = clonedElement.getElementsByTagName("p")[0];
+          const textElement = clonedElement.getElementsByTagName("span")[0];
           textElement.innerText = "결정화 캐즘";
           clonedElement.setAttribute("href", "javascript: void(0)");
           clonedElement.onclick = (event) => {
@@ -1220,7 +1277,7 @@ if (!document.chasmApi) {
         if (element.getAttribute("href") === "/my-page") {
           const clonedElement = element.cloneNode(true);
           clonedElement.id = "chasm-decentral-menu";
-          const textElement = clonedElement.getElementsByTagName("p")[0];
+          const textElement = clonedElement.getElementsByTagName("span")[0];
           textElement.innerText = "결정화 캐즘";
           clonedElement.setAttribute("href", "javascript: void(0)");
           clonedElement.onclick = (event) => {
