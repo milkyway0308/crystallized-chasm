27150119d7f728455abb4debf7273855199c53d6
skywolf46 <skywolf461@gmail.com>
2026-04-06 13:10:53 +0900

    feat: 공용 일반 사용 타입 추가

---
+++ src/utils/generic-types.ts
@@ -0,0 +1,22 @@
+export type AttachedDocument<T> = Document & { __attached?: T };
+export type ExpandedVoid = void | Promise<void>;
+
+export type Runnable = () => void;
+export type AsyncRunnable = () => ExpandedVoid;
+
+export type Consumer<P> = (parameter: P) => void;
+export type AsyncConsumer<P> = (parameter: P) => ExpandedVoid;
+
+export type BiConsumer<F, S> = (first: F, second: S) => void;
+export type AsyncBiConsumer<F, S> = (first: F, second: S) => ExpandedVoid;
+
+
+export type TriConsumer<F, S, T> = (first: F, second: S, third: T) => void;
+export type AsyncTriConsumer<F, S, T> = (first: F, second: S, third: T) => ExpandedVoid;
+
+export type Processor<I, O> = (input: I) => O
+export type AsyncProcessor<I, O> = (input: I) => Promise<O>
+
+
+export type Nullable<T> = T | null;
+export type Skippable<T> = T | undefined | void;
