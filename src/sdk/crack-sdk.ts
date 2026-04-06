9f56d14efa47851615b1982c30657cad6bcbdba2
skywolf46 <skywolf461@gmail.com>
2026-04-07 00:20:52 +0900

    feat: 코어 SDK 재조정

--- src/sdk/crack-sdk.ts
+++ src/sdk/crack-sdk.ts
@@ -1,6 +1,8 @@
 import { CrackAttendApi } from "./crack/attend-util";
+import { CrackComponentApi } from "./crack/component-util";
 import { CrackCookieApi } from "./crack/cookie-util";
 import { CrackCrackerApi } from "./crack/cracker-util";
+import { CrackEnvironmentUtil as CrackEnvironmentApi } from "./crack/environment-util";
 import { CrackNetworkApi } from "./crack/network-util";
 import { CrackPathApi } from "./crack/path-util";
 import { CrackThemeApi } from "./crack/theme-util";
@@ -9,6 +11,8 @@ export class CrackSdk {
   static readonly cookieApi = new CrackCookieApi();
   static readonly themeApi = new CrackThemeApi();
   static readonly pathApi = new CrackPathApi();
+  static readonly environmentApi = new CrackEnvironmentApi();
+  static readonly componentApi = new CrackComponentApi();
   static readonly networkApi = new CrackNetworkApi(this.cookieApi);
   static readonly attendApi = new CrackAttendApi(this.networkApi);
   static readonly crackerApi = new CrackCrackerApi(this.networkApi);
@@ -19,6 +23,22 @@ export class CrackSdk {
    */
   static attend(): CrackAttendApi {
     return this.attendApi;
+  }
+
+  /**
+   * 크랙 페이지 환경 유틸리티를 반환합니다.
+   * @returns 환경 유틸리티
+   */
+  static environment(): CrackEnvironmentApi {
+    return this.environmentApi;
+  }
+
+  /**
+   * 크랙 페이지 컴포넌트 유틸리티를 반환합니다.
+   * @returns 컴포넌트 유틸리티
+   */
+  static component(): CrackComponentApi {
+    return this.componentApi;
   }
 
   /**
