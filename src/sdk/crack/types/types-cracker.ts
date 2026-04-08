bd0478501b1aa7a801af53f22d66f06373db36b2
skywolf46 <skywolf461@gmail.com>
2026-04-08 22:10:26 +0900

    chore: 필드 인라인으로 변경

--- src/sdk/crack/types/types-cracker.ts
+++ src/sdk/crack/types/types-cracker.ts
@@ -2,21 +2,13 @@
  * 크래커 모델의 비용과 이름을 나타내는 클래스입니다.
  */
 export class CrackerModel {
-  /** 모델 내부 ID */
-  id: string;
-  /** 표시 이름 */
-  name: string;
-  /** 1회당 소모 크래커 */
-  quantity: string;
-  /** 서비스 타입 */
-  serviceType;
   /**
    * @param id 모델 내부 ID
    * @param name 표시 이름
    * @param quantity 1회당 소모 크래커
    * @param serviceType 서비스 타입
    */
-  constructor(id: string, name: string, quantity: string, serviceType: string) {
+  constructor(public readonly id: string, public readonly name: string, public readonly quantity: number, public readonly serviceType: string) {
     this.id = id;
     this.name = name;
     this.quantity = quantity;
