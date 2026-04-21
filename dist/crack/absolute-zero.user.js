ec60460c2c0eb907894936962b26a7e1f321d043
skywolf46 <skywolf461@gmail.com>
2026-04-21 16:19:32 +0900

    chore: 컴파일 부산물 커밋

--- dist/crack/absolute-zero.user.js
+++ dist/crack/absolute-zero.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Chasm Crystallized AbsoluteZero (결정화 캐즘 절대영도)
 // @namespace    https://github.com/milkyway0308
-// @version      CRCK-ALRM-v2.0.0
+// @version      CRCK-AZRO-v2.0.0
 // @author       milkyway0308
 // @description  선택하거나 호버하지 않은 작품의 GIF 차단. 이 기능은 결정화 캐즘 오리지널 패치입니다.
-// @downloadURL  https://github.com/milkyway0308/crystallized-chasm/dist/crack/alarm-clock.user.js
-// @updateURL    https://github.com/milkyway0308/crystallized-chasm/dist/crack/alarm-clock.user.js
+// @downloadURL  https://github.com/milkyway0308/crystallized-chasm/dist/crack/absolutezero.user.js
+// @updateURL    https://github.com/milkyway0308/crystallized-chasm/dist/crack/absolutezero.user.js
 // @match        https://crack.wrtn.ai/*
 // @grant        GM_addStyle
 // ==/UserScript==
